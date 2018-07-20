5c5e36adb4770a812e04d2ce7342bc3ad9b35a43
mahmoudelhalwany <mahmoud.a.elhalwany@gmail.com>
2018-07-20 14:52:04 +0200

    ploynomial regression

--- predict/prediction.py
+++ predict/prediction.py
@@ -72,7 +72,23 @@ class prediction:
      def ploynomial_reg():
         #return error 
         # ploynomial regression with its all possible degrees and return the best one 
-        
+        ploy_error=100
+        lin_regressor
+        m=dataset.shape[0]
+        for i in range (1:m):
+            lin_regressor = LinearRegression()
+            poly = PolynomialFeatures(i)
+            X_transform = poly.fit_transform(train[predict_columns])
+            lin_regressor.fit(X_transform,y_train[target]) 
+            y_preds = lin_regressor.predict(test[predict_columns])
+            m_error=mean_squared_error(y_preds,test[target])
+            if(m_error+0.0001<ploy_error):
+                ploy_error=m_error
+            else:
+                break
+        if(ploy_error<error):
+            pre_dict=lin_regressor
+            
         
         
         
