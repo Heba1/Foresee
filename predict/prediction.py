871aac57b2b57d814e6170a81ec4d9cc0d32ba71
hind <hind@hind.local>
2018-07-20 17:58:26 +0300

    check,clean,prepare functions

--- predict/prediction.py
+++ predict/prediction.py
@@ -9,7 +9,8 @@ import seaborn as sb
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
-
+from pandas.api.types import is_numeric_dtype
+from pandas.api.types import is_string_dtype
 
 #class prediction
 class prediction:
@@ -39,15 +40,33 @@ class prediction:
         #check validation of data
         # make checked_data true for valid or false for not valid
         #return"valid or not valid "
+        if (dataset.isnull().sum().sum() == 0) and is_numeric_dtype(dataset['job_date']) and is_string_dtype(dataset['job_location']) and is_string_dtype(dataset['job_title']): #ceck null row (AND) check column type:
+            return "vlaid"
+        else: 
+            return "not vlaid"
+        
         
     def clean_data():
-        
         #delete null rows 
         # return deleted rows and the new dataset after delete rows
+        data_state  = check_data()
+        if data_state == "not vlaid":
+            #delet nill row from dataset
+            dataset = dataset[dataset!=None]
+            dataset = dataset.dropna(axis = 0)
+            # convert column type
+            dataset.job_date = dataset.job_date.astype(int)
+            dataset.job_date = dataset.job_location.astype(str)
+            dataset.job_date = dataset.job_title.astype(str)
+        #return the dataset after cleaning
+        return dataset
         
     def prepare():
         #split data to training and test 
         # return boolen "true "if done "false " if not 
+        from sklearn.cross_validation import train_test_split
+        train=dataset.sample(frac=0.8,random_state=1)
+        test=dataset.loc[~ dataset.index.isin(train.index)]
         
         
     def best_predict():
@@ -65,11 +84,12 @@ class prediction:
         
         
         
-     def RF_reg():
+    def RF_reg():
         #return error
         #random forest 
         
-     def ploynomial_reg():
+        
+    def ploynomial_reg():
         #return error 
         # ploynomial regression with its all possible degrees and return the best one 
         ploy_error=100
